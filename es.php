12522fa87a86e616fa4fe15eb7cee3b671a27a44
unknown <2_web9_19@JOVELLANOS.EDU>
2019-02-07 20:33:52 +0100

    Terminada pagina php

---
+++ es.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Lenguajes</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+</head>
+  <body>
+    <?php include("nav.inc")?>
+    <div class="container">
+       <div class="jumbotron">
+          <h1 class="display-4">ECMAScript</h1>
+          <p class="lead">La moda, la princesita favorita de todo programador, siempre comprando vestidos nuevos para ir a la ultima y cuando te traen el vestido ya se ha pasado de moda, nadie la quiere desnuda porque es horrorsa hasta decir basta.</p>
+          <hr class="my-4">
+          <p>Se llama Ecma, pero todo el mundo la conoce por el nombre de su prima (lejana).</p>
+        </div>
+        <div class="article">
+            <h1>ECMAScript</h1>
+            <p><img alt="java logo" src="img/ecmascript-logo-bf5110fcdc155bf03a62c7863573ec65-21213.png" class="float-right rounded" style="width: 200px;">especificación de lenguaje de programación publicada por ECMA International. El desarrollo empezó en 1996 y estuvo basado en el popular lenguaje JavaScript propuesto como estándar por Netscape Communications Corporation. Actualmente está aceptado como el estándar ISO 16262.</p><p>
+            ECMAScript define un lenguaje de tipos dinámicos ligeramente inspirado en Java y otros lenguajes del estilo de C. Soporta algunas características de la programación orientada a objetos mediante objetos basados en prototipos y pseudoclases.</p><p>
+            La mayoría de navegadores de Internet incluyen una implementación del estándar ECMAScript, al igual que un acceso al Document Object Model para manipular páginas web. JavaScript está implementado en la mayoría de navegadores, Internet Explorer de Microsoft usa JScript. El navegador Opera tenía su propio intérprete de ECMAScript con extensiones para soportar algunas características de JavaScript y JScript, actualmente Opera esta basado en Chromium (y utiliza su intérprete). Cada navegador tiene extensiones propias al estándar ECMAScript, pero cualquier código que se adecúe al estándar debería funcionar en todos ellos.</p><p>
+            ActionScript, para Adobe Flash, también está basado en el estándar ECMAScript, con mejoras que permiten mover, crear y analizar dinámicamente objetos, mientras la película está en ejecución.</p><p>
+            Desde el lanzamiento en junio de 1997 del estándar ECMAScript 1, han existido las versiones 2, 3 y 5, que es la más usada actualmente (la 4 se abandonó2​). En junio de 2015 se cerró y publicó la versión ECMAScript 6.3​ </p>
+            <a href="https://developer.mozilla.org/es/docs/Web/JavaScript"><button type="button" class="btn btn-outline-primary">Documentacion EcmaScript 6</button></a>
+            <a href="https://jquery.com/"><button type="button" class="btn btn-outline-success">Descargar JQuery</button></a>
+        </div>
+    </div>
+    <?php include("footer.inc")?>
+    
+  </body>
+</html>
